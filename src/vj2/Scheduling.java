cfa0e74615ec12634a1811fd62aa8fb533228797
milannovic <116819818+milannovic@users.noreply.github.com>
2024-11-15 12:03:43 +0100

    commit vj2

--- src/vj2/Scheduling.java
+++ src/vj2/Scheduling.java
@@ -1,9 +1,6 @@
 package vj2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 class Process{
     int id;
@@ -15,12 +12,12 @@ class Process{
         this.burst = burstTime;
         this.remaining = burstTime;
     }
-    /*Process(int id,float burst,float remaining, int priority){
+    Process(int id,float burst,float remaining, int priority){
         this.id=id;
         this.burst=burst;
         this.remaining=remaining;
         this.priority=priority;
-    }*/
+    }
 
     @Override
     public String toString() {
@@ -29,21 +26,20 @@ class Process{
 }
 public class Scheduling{
     static ArrayList<Process> listProcess = new ArrayList<Process>(Arrays.<Process>asList(
-            new Process(3, 5),
-            new Process(4,8),
+            new Process(3, 8),
+            new Process(4,4),
             new Process(1, 7),
             new Process(5, 5),
-            new Process(2, 3)
+            new Process(2, 2)
     ));
 
     public static void main(String[] args) {
-        jsfScheduling();
+        sjfScheduling();
+        rrScheduling(4);
     }
-    static void jsfScheduling(){
+    static void sjfScheduling(){
         Queue<Process> queue=new PriorityQueue<>(
-                (p1,p2)->{
-                    return Float.compare(p1.burst, p2.burst);
-                });
+                (p1,p2)-> Float.compare(p1.burst, p2.burst));
         queue.addAll(listProcess);
         /*for (Process p : listProcess)
             queue.add(p);
@@ -57,6 +53,35 @@ public class Scheduling{
                 System.out.println(e);
             }*/
         }
+    }
+    static void rrScheduling(int quantum){
+        Queue<Process> queue=new LinkedList<>();
+        for (Process p : listProcess)
+            queue.add(p);
+        //int numAdd=1;
+        while (!queue.isEmpty()){
+            Process current=queue.poll();
+            float runningTime=Math.min(quantum,current.remaining);
+            System.out.println("Izvrsava se proces: "+current+" "+runningTime+" s");
+            try{
+                Thread.sleep((int)runningTime*500);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            current.remaining-=runningTime;
+            /*if(numAdd>0){
+                numAdd--;
+                listProcess = new ArrayList<Process>(Arrays.asList(
+                        new Process(3, 8),
+                        new Process(4, 4)
+                        ));
+                for(Process p: listProcess)
+                    queue.add(p);
+            }*/
+            if(current.remaining>0)
+                queue.add(current);
+        }
+
     }
 
 }
